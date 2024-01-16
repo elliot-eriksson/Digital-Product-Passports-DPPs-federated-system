095bed686e81acde5add20b673b51f21140dfc27
sunlar-3 <sunlar-3@student.ltu.se>
2024-01-16 15:12:50 +0100

    changes to connection to DB Elliot Eriksson <elliot-eriksson@users.noreply.github.com> Samuel Thelin <samthe-1@student.ltu.se>

--- ConnectDatabase/connectDatabase.go
+++ ConnectDatabase/connectDatabase.go
@@ -10,7 +10,51 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
+func connectDatabase(database string, collection string, itemID int) (client *mongo.Client, err error) {
+	var username, password string = "TestComp1", "TestComp1"
+	fmt.Println("hej")
+
+	// fmt.Println("Username: ")
+	// fmt.Scanln(&username)
+	// fmt.Println("Password: ")
+	// fmt.Scanln(&password)
+	// var database string = "Test"
+	// var collection string = username
+	// fmt.Println("ItemID: ")
+	// fmt.Scanln(&itemID)
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	// opts := options.Client().ApplyURI("mongodb+srv://" + username + ":" + password + "@digital-product-passpor.mjd4fio.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
+	// gamla här under
+	opts := options.Client().ApplyURI("mongodb+srv://" + username + ":" + password + "@cluster0.qk8pnen.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client2, err2 := mongo.Connect(context.TODO(), opts)
+	fmt.Println("client innan", client2)
+	if err2 != nil {
+		fmt.Println("hej")
+		panic(err2)
+	}
+	defer func() {
+		if err = client2.Disconnect(context.TODO()); err2 != nil {
+			panic(err2)
+		}
+	}()
+	return client2, err2
+
+	// var keyPhrase string = "hej"
+	// fmt.Println("-------------string format...............")
+	// fmt.Println(string(encryptIt([]byte(jsonData), keyPhrase)))
+	// fmt.Println("-------------Decrypt format...............")
+	// fmt.Println(string(decryptIt(encryptIt([]byte(jsonData), keyPhrase), keyPhrase)))
+
+	// // Send a ping to confirm a successful connection
+	// if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+	//  panic(err)
+	// }
+	// fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+}
+
+func connectToDatabase2() {
 	var username, password string = "TestComp1", "TestComp1"
 	var itemID int
 	// fmt.Println("Username: ")
@@ -28,6 +72,7 @@ func main() {
 	opts := options.Client().ApplyURI("mongodb+srv://" + username + ":" + password + "@cluster0.qk8pnen.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
+	print("client", client)
 	if err != nil {
 		panic(err)
 	}
@@ -36,9 +81,11 @@ func main() {
 			panic(err)
 		}
 	}()
+
 	coll := client.Database(database).Collection(collection)
 	var result bson.M
 	err = coll.FindOne(context.TODO(), bson.D{{"ItemID", itemID}}).Decode(&result)
+	//Error handling
 	if err == mongo.ErrNoDocuments {
 		fmt.Printf("No item with item %s\n", itemID)
 	}
@@ -49,17 +96,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Printf("json %s\n", jsonData)
-	// fmt.Printf("bson %s\n", result["Origin"])
-	var keyPhrase string = "hej"
-	fmt.Println("-------------string format...............")
-	fmt.Println(string(encryptIt([]byte(jsonData), keyPhrase)))
-	fmt.Println("-------------Decrypt format...............")
-	fmt.Println(string(decryptIt(encryptIt([]byte(jsonData), keyPhrase), keyPhrase)))
-
-	// // Send a ping to confirm a successful connection
-	// if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-	//  panic(err)
-	// }
-	// fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	fmt.Printf("json %s\n", jsonData)
+	fmt.Printf("bson %s\n", result["Origin"])
 }
+
+// func splitPassports() {
+
+// }

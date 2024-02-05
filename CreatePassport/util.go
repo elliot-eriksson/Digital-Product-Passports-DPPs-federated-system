a46dfc4643663c0f4a22901a68161b93ef0f2d10
sunlar-3 <sunlar-3@student.ltu.se>
2024-02-05 12:32:17 +0100

    more cleanup

--- CreatePassport/util.go
+++ CreatePassport/util.go
@@ -24,10 +24,6 @@ func performChecks(sh *shell.Shell) error {
 		return fmt.Errorf("'YourLocalPath' constant is NOT defined. Please, provide a local path in your computer where the file will be downloaded")
 	}
 
-	if YourPublicKey == "" {
-		return fmt.Errorf("'YourPublicKey' constant is NOT defined. Please, provide the public key of your IPFS node")
-	}
-
 	if !sh.IsUp() {
 		return fmt.Errorf("You do not have an IPFS node running at port 5001")
 	}
